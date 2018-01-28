ad0938f78e118c4cc3a3a413029988ea54d8fa7d
Yosadaq Esparza <yosadaq.esparza@yahoo.com>
2018-01-28 10:58:54 -0600

    Created a struct and passed data through it

--- Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Controller/LeagueVC.swift
@@ -10,15 +10,41 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var Next: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
 
-
+    }
+    @IBAction func OnCoedtapped(_ sender: Any) {
+        selectLeague(LeagueType: "Coed")
+        
+    }
+    @IBAction func OnMensTapped(_ sender: Any) {
+        selectLeague(LeagueType: "Mens")
+        
+    }
+    @IBAction func OnWomensTapped(_ sender: Any) {
+        selectLeague(LeagueType: "Womens")
+        
     }
     
     @IBAction func Next(_ sender: Any) {
         performSegue(withIdentifier: "SkillVCSegue", sender: self)
     }
-
+    func selectLeague(LeagueType: String) {
+    player.desiredLeague = LeagueType
+    Next.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
+    
 }
---
+++ Swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+    //
+//  Player.swift
+//  Swoosh
+//
+//  Created by Yosadaq Esparza on 1/27/18.
+//  Copyright © 2018 Y.M. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+        var desiredLeague: String!
+        var selectedSkill: String!
+        
+    }
